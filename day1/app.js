60e83c57fb84b727c34f2509859a704327226e61
bibhukiju <bibhukiju@gmail.com>
2020-11-01 00:53:53 +0545

    queryselector and get elementbyid

---
+++ day1/app.js
@@ -0,0 +1,52 @@
+//slect the html elements  by its class ->return all elemets of that tag
+const header = document.getElementsByTagName("h2");
+console.log(header);
+//slect the html elements  by its class name
+const tester = document.getElementsByClassName("item");
+console.log(tester);
+//select the html element by id ->returns the first element
+const homework = document.getElementById("hello");
+console.log(homework);
+
+const treaders = document.querySelector("h2");
+//select only one with that name
+console.log(treaders);
+const breaders = document.querySelectorAll(".item");
+//select all the elements with that name
+console.log(breaders);
+const readers = document.querySelectorAll("#list a");
+//only selects the a tag of the parent (ie list)
+console.log(readers);
+
+//looping in quryselector
+for (items of breaders) {
+  console.log(items);
+}
+for (items of tester) {
+  console.log(items);
+}
+// adding new elements using HTML collections
+const collectionItems = document.getElementsByClassName("item");
+const list = document.getElementById("list");
+const newItem = document.createElement("li");
+const number = document.getElementsByClassName("hello")[0];
+newItem.classList.add("item");
+newItem.innerText = "Item 6";
+
+list.appendChild(newItem);
+
+number.innerText = collectionItems.length;
+
+//adding new elemts using query selector
+const collectin=document.querySelectorAll('.item');
+const qlist = document.querySelector("#list");
+number = document.querySelector("hello");
+
+const newItema = document.createElement("li");
+newItema.classList.add("item");
+newItema.innerText = "Item 9";
+qlist.appendChild(newItema);
+number.innerText=collectin.length;
+
+
+//Events
