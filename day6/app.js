7e062d22192ef67b7df6d670e4e54b38f379fd4d
bibhukiju <bibhukiju@gmail.com>
2020-11-06 12:15:02 +0545

    graph canvas is initiated

--- day6/app.js
+++ day6/app.js
@@ -4,10 +4,15 @@ const recentransDiv = document.querySelector(".rcntTrans");
 const addtrans = document.querySelector(".add-transaction");
 const popWindow = document.querySelector(".pop-window");
 const closePop = document.querySelector(".close");
+const canvas = document.querySelector(".graphcanvas");
 
 //eventListeners
 addtrans.addEventListener("click", addtransaction);
-closePop.addEventListener("click",closePopwindow)
+closePop.addEventListener("click", closePopwindow);
+
+//global variables;
+let totalInc = 0;
+let totalExp = 0;
 
 //class
 class Transaction {
@@ -24,22 +29,26 @@ allTransList = [
   { type: "Income", desc: "homies", amount: 10 },
   { type: "Income", desc: "homies", amount: 10 },
   { type: "Income", desc: "homies", amount: 10 },
+  { type: "Expenses", desc: "my  expense", amount: 80 },
   { type: "Income", desc: "last4", amount: 10 },
   { type: "Income", desc: "test", amount: 10 },
+  { type: "Expenses", desc: "my 2nd expense", amount: 80 },
 ];
 
 //functions
 function createdataSummary() {
-  const arr = [1, 2, 3, 4, 5];
-  const earr = [1, 5, 6, 8];
   let totalinc = 0;
   let totalexp = 0;
-  arr.forEach((element) => {
-    totalinc += parseInt(element);
+  allTransList.forEach((element) => {
+    if (element.type == "Income") {
+      totalinc += element.amount;
+      totalInc = totalinc;
+    } else {
+      totalexp += element.amount;
+      totalExp = totalexp;
+    }
   });
-  earr.forEach((element) => {
-    totalexp += parseInt(element);
-  });
+
   //total div
   const totalDiv = document.createElement("div");
   totalDiv.style.backgroundColor = "#EBEBEB";
@@ -84,6 +93,10 @@ function createdataSummary() {
   expDiv.appendChild(expAmount);
   expDiv.classList.add("summaryDiv");
   dataSumary.appendChild(expDiv);
+  return {
+    total_inc: totalinc,
+    total_exp: totalexp,
+  };
 }
 function recentransactionBlocks() {
   for (
@@ -93,6 +106,9 @@ function recentransactionBlocks() {
   ) {
     console.log(allTransList[element]);
     const listTile = document.createElement("div");
+    if (allTransList[element].type != "Income") {
+      listTile.style.color = "red";
+    }
     listTile.classList.add("trnList");
     recentransDiv.appendChild(listTile);
     const typeDiv = document.createElement("div");
@@ -112,14 +128,24 @@ function recentransactionBlocks() {
   }
 }
 function addtransaction() {
-  console.log("hello");
   popWindow.style.display = "flex";
 }
-function closePopwindow()
-{
-  console.log("close it")
+function closePopwindow() {
   popWindow.style.display = "none";
+}
+function drawGraph() {
+  const ctx = canvas.getContext("2d");
+  canvas.height = 400;
+  canvas.width = 350;
+  ctx.beginPath();
+  ctx.moveTo(20, 20);
+  ctx.lineTo(20, 400);
+  ctx.lineTo(390, 400);
+  ctx.stroke();
+  let yIncr = totalInc > totalExp ? totalInc / 350 : totalExp / 350;
+  console.log(yIncr);
 }
 
 createdataSummary();
 recentransactionBlocks();
+drawGraph();
