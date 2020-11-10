09d041dc3b547fdcc6b33ce2815196f2863d12b0
bibhukiju <bibhukiju@gmail.com>
2020-11-10 18:01:09 +0545

    response is send fromm node

--- day10/app.js
+++ day10/app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node</title></head>");
+  res.write("<body><h1>Bibhu</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
