e140ff4c4d6e7c9112c79029ecd8ce9c77a8a56c
bibhukiju <bibhukiju@gmail.com>
2020-11-27 22:58:08 +0545

    day21

--- day20/src/middleware/auth.js
+++ day20/src/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, "hellothisisme");
+    const decoded = jwt.verify(to ken, "hellothisisme");
     console.log(decoded._id);
     const user = await User.findOne({
       _id: decoded._id,
