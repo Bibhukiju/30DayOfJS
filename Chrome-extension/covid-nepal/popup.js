cd45ab93355a3c0a63fc3431737975ca1f19b996
bibhukiju <bibhukiju@gmail.com>
2020-11-30 12:09:09 +0545

    promise is changed to async await

--- Chrome-extension/covid-nepal/popup.js
+++ Chrome-extension/covid-nepal/popup.js
@@ -3,22 +3,18 @@ const deathDiv = document.querySelector(".death");
 const testedPositiveDiv = document.querySelector(".positive");
 const recoveredDiv = document.querySelector(".recovered");
 const updatedDiv = document.querySelector(".updatedat");
+
 //eventListeners
 document.addEventListener("DOMContentLoaded", getData);
 
 //functions
-function getData() {
-  fetch("https://nepalcorona.info/api/v1/data/nepal")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      deathDiv.innerHTML = `<h3 class="data">${data.deaths}</h3>`;
-      testedPositiveDiv.innerHTML = `<h3 class="data">${data.tested_positive}</h3>`;
-      recoveredDiv.innerHTML = `<h3 class="data">${data.recovered}</h3>`;
-      let updatedTime = data.latest_sit_report.updated_at;
-      console.log(typeof updatedTime);
-      updatedTime = updatedTime.split("T")[0];
-      updatedDiv.innerHTML = `<h2>${updatedTime}</h2>`;
-    });
+async function getData() {
+  let data = await fetch("https://nepalcorona.info/api/v1/data/nepal");
+  data = await data.json();
+  deathDiv.innerHTML = `<h3 class="data">${data.deaths}</h3>`;
+  testedPositiveDiv.innerHTML = `<h3 class="data">${data.tested_positive}</h3>`;
+  recoveredDiv.innerHTML = `<h3 class="data">${data.recovered}</h3>`;
+  let updatedTime = data.latest_sit_report.updated_at;
+  updatedTime = updatedTime.split("T")[0];
+  updatedDiv.innerHTML = `<h2>${updatedTime}</h2>`;
 }
